797b9a04cb529fb18437b79a53e9d635e625e47c
Ayoub bakali <ayoubbakali817@gmail.com>
2025-05-24 17:38:07 +0100

    fix auth redirect

--- UI_UX_Design project UI/app/Http/Controllers/Auth/RegisterController.php
+++ UI_UX_Design project UI/app/Http/Controllers/Auth/RegisterController.php
@@ -28,7 +28,7 @@ class RegisterController extends Controller
      *
      * @var string
      */
-    protected $redirectTo = '/home';
+    protected $redirectTo = '/formateur/dashboard';
 
     /**
      * Create a new controller instance.
